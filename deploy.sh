c5ea2d4fc7c57c873e81e890c1e8ec6d2823597b
Edgar Powell <epowelljr2@gmail.com>
2026-04-14 11:09:25 -0400

    feat: finalize deployment config with production remote

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # Talovi deployment script
 # Usage: ./deploy.sh
+set -e
 
 echo "Deploying Talovi to talovi.dev..."
 git push production main
 echo "Running Composer on server..."
-ssh talovi-chemicloud "cd public_html && composer install --no-dev --optimize-autoloader"
+ssh talovi-chemicloud "cd /home/talovi/public_html && composer install --no-dev --optimize-autoloader"
 echo "Clearing Drupal cache..."
-ssh talovi-chemicloud "cd public_html && vendor/bin/drush cr"
+ssh talovi-chemicloud "cd /home/talovi/public_html && vendor/bin/drush cr"
 echo "Deployment complete."
